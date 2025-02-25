f5bf419efb9cfe28596fe636411b3d8ef2910cae
rymsha <sry@enonic.com>
2025-02-25 12:39:47 +0100

    Don't use Jackson from XP API

--- src/main/java/com/enonic/app/livetrace/metrics/ClusterInfoReporter.java
+++ src/main/java/com/enonic/app/livetrace/metrics/ClusterInfoReporter.java
@@ -1,18 +1,22 @@
 package com.enonic.app.livetrace.metrics;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.enonic.xp.status.JsonStatusReporter;
 import com.enonic.xp.status.StatusReporter;
 
 @Component(immediate = true, service = {ClusterInfoReporter.class})
 public class ClusterInfoReporter
 {
-    private JsonStatusReporter jsonReporter;
+    private StatusReporter jsonReporter;
 
     public ClusterInfoReporter()
     {
@@ -23,7 +27,7 @@ public class ClusterInfoReporter
     {
         if ( reporter.getName().equals( "cluster" ) || reporter.getName().equals( "cluster.elasticsearch" ) )
         {
-            this.jsonReporter = (JsonStatusReporter) reporter;
+            this.jsonReporter = reporter;
         }
     }
 
@@ -32,7 +36,18 @@ public class ClusterInfoReporter
         ClusterInfo info = new ClusterInfo();
         if ( jsonReporter != null )
         {
-            final JsonNode json = jsonReporter.getReport();
+            final JsonNode json;
+            try
+            {
+                final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                jsonReporter.report( outputStream );
+                json = new ObjectMapper().reader().readTree( outputStream.toByteArray() );
+            }
+            catch ( IOException e )
+            {
+                throw new UncheckedIOException( e );
+            }
+
             final JsonNode localNode = json.get( "localNode" );
             info.name = json.get( "name" ).asText();
             info.state = json.get( "state" ).asText();
